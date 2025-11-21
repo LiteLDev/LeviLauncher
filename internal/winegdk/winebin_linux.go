173df8eb0d7dcbed1ba02c8e84d353d1547b3928
Esczvefs <guxiang@litebds.com>
2025-11-21 20:35:59 +0800

    feat: linux(WIP)

--- internal/winegdk/winebin_linux.go
+++ internal/winegdk/winebin_linux.go
@@ -3,23 +3,28 @@
 package winegdk
 
 import (
-    "os"
-    "os/exec"
-    "path/filepath"
-    "strings"
-    "github.com/liteldev/LeviLauncher/internal/utils"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/liteldev/LeviLauncher/internal/utils"
 )
 
 func FindWineBin() string {
-    app := strings.TrimSpace(utils.GetAppDataPath())
-    if app != "" {
-        p := filepath.Join(app, "wine", "files", "bin", "wine")
-        if fi, err := os.Stat(p); err == nil && fi.Mode().IsRegular() { return p }
-        wow := filepath.Join(app, "wine", "files", "bin-wow64", "wine")
-        if fi, err := os.Stat(wow); err == nil && fi.Mode().IsRegular() { return wow }
-        alt := filepath.Join(app, "wine", "files", "bin", "wine64")
-        if fi, err := os.Stat(alt); err == nil && fi.Mode().IsRegular() { return alt }
-    }
-    if sys, err := exec.LookPath("wine"); err == nil { return sys }
-    return ""
+	app := strings.TrimSpace(utils.GetAppDataPath())
+	if app != "" {
+		p := filepath.Join(app, "wine", "files", "bin", "wine")
+		if fi, err := os.Stat(p); err == nil && fi.Mode().IsRegular() {
+			return p
+		}
+		wow := filepath.Join(app, "wine", "files", "bin-wow64", "wine")
+		if fi, err := os.Stat(wow); err == nil && fi.Mode().IsRegular() {
+			return wow
+		}
+		alt := filepath.Join(app, "wine", "files", "bin", "wine64")
+		if fi, err := os.Stat(alt); err == nil && fi.Mode().IsRegular() {
+			return alt
+		}
+	}
+	return ""
 }

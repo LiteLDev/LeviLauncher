f505d7433b167bfdec3ff4618f39fe585daa06a6
Esczvefs <guxiang@litebds.com>
2025-12-13 13:01:13 +0800

    feat: add Russian language support

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -3,6 +3,7 @@ import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import en from "./assets/locales/en_US.json";
 import zh from "./assets/locales/zh_CN.json";
+import ru from "./assets/locales/ru_RU.json";
 
 export const resources = {
   en_US: { translation: en },
@@ -11,6 +12,9 @@ export const resources = {
   zh_CN: { translation: zh },
   "zh-CN": { translation: zh },
   zh: { translation: zh },
+  ru_RU: { translation: ru },
+  "ru-RU": { translation: ru },
+  ru: { translation: ru },
 };
 
 i18n
@@ -20,7 +24,17 @@ i18n
     resources,
     load: "currentOnly",
     fallbackLng: "en_US",
-    supportedLngs: ["en_US", "en-US", "en", "zh_CN", "zh-CN", "zh"],
+    supportedLngs: [
+      "en_US",
+      "en-US",
+      "en",
+      "zh_CN",
+      "zh-CN",
+      "zh",
+      "ru_RU",
+      "ru-RU",
+      "ru",
+    ],
     lowerCaseLng: false,
     nonExplicitSupportedLngs: true,
     detection: {

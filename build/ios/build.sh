a195f1459f6ea49e6469d949e34729ea38a486d6
Esczvefs <guxiang@litebds.com>
2026-03-23 15:59:40 +0800

    chore(build): bump version to 0.3.9

--- build/ios/build.sh
+++ build/ios/build.sh
@@ -4,8 +4,8 @@ set -e
 # Build configuration
 APP_NAME="LeviLauncher"
 BUNDLE_ID="org.levimc.launcher"
-VERSION="0.3.8"
-BUILD_NUMBER="0.3.8"
+VERSION="0.3.9"
+BUILD_NUMBER="0.3.9"
 BUILD_DIR="build/ios"
 TARGET="simulator"
 
